a4d9910366b7b1295eba3f05d4e7b4e0c92fffab
Strongartt <rikardoperez0809@gmail.com>
2024-06-30 23:54:36 -0500

    Agregar archivos de proyecto.

---
+++ People/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+
+namespace People.Models
+{
+    [Table("people")]
+
+        public class Person
+    {
+
+        [PrimaryKey, AutoIncrement]
+
+        public int Id  { get; set; }
+
+        [MaxLength(250), Unique]
+
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string Ciudad { get; set; }
+}
+}
+
